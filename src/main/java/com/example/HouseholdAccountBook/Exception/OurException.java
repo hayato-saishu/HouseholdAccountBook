6e700ddf88fe6d872021932707ed86ca1574c689
saishuhayato <hayato.saishu@rakus.partners.co.jp>
2024-11-01 01:00:09 +0900

    Exceptionの新規作成

---
+++ src/main/java/com/example/HouseholdAccountBook/Exception/OurException.java
@@ -0,0 +1,7 @@
+package com.example.HouseholdAccountBook.Exception;
+
+public class OurException extends RuntimeException {
+    public OurException(String message) {
+        super(message);
+    }
+}
