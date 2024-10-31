b4fcd577c56e158c864c279ea750f8a33f0bd8f1
saishuhayato <hayato.saishu@rakus.partners.co.jp>
2024-10-31 23:56:14 +0900

    パッケージ名を小文字に修正

--- src/main/java/com/example/HouseholdAccountBook/controller/IndexController.java
+++ src/main/java/com/example/HouseholdAccountBook/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.example.HouseholdAccountBook.Controller;
+package com.example.HouseholdAccountBook.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
