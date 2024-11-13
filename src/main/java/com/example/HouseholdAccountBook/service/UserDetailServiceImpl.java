e323427daddd280ed41cc288a406444d4144786f
saishuhayato <hayato.saishu@rakus.partners.co.jp>
2024-11-13 23:19:33 +0900

    UserDetailService作成

---
+++ src/main/java/com/example/HouseholdAccountBook/service/UserDetailServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.HouseholdAccountBook.service;
+
+import com.example.HouseholdAccountBook.Exception.OurException;
+import com.example.HouseholdAccountBook.entity.User;
+import com.example.HouseholdAccountBook.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        try {
+            User user = userRepository.findByEmail(username).orElseThrow(() -> new OurException("Username/メールアドレスが見つかりませんでした。"));
+            return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("ユーザーが見つかりませんでした。");
+        }
+    }
+}
