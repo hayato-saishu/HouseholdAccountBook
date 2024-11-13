43f59f0c6b1aea742838f79d375e0836b3e1d34a
saishuhayato <hayato.saishu@rakus.partners.co.jp>
2024-11-14 00:40:06 +0900

    AuthResponseDto作成

---
+++ src/main/java/com/example/HouseholdAccountBook/dto/AuthResponseDto.java
@@ -0,0 +1,11 @@
+package com.example.HouseholdAccountBook.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthResponseDto {
+    private String accessToken;
+    public AuthResponseDto(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
