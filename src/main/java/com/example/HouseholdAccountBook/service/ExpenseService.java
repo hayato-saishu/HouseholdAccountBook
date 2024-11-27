444c155d3ce5b1714c60006dc52546f7a58cb24f
saishuhayato <hayato.saishu@rakus.partners.co.jp>
2024-11-27 21:58:15 +0900

    家計簿の明細取得のAPI作成

--- src/main/java/com/example/HouseholdAccountBook/service/ExpenseService.java
+++ src/main/java/com/example/HouseholdAccountBook/service/ExpenseService.java
@@ -3,6 +3,7 @@ package com.example.HouseholdAccountBook.service;
 import com.example.HouseholdAccountBook.dto.Response;
 
 public interface ExpenseService {
+    Response getExpenseByUserId(String userId);
     Response getExpenseByUserIdAndMonth(String userId, String month);
     Response getExpenseByUserIdAndCategoryId(String userId, String categoryId);
     Response getByUserIdAndCategoryIdAndMonth(String userId, String categoryId, String month);
--- src/main/java/com/example/HouseholdAccountBook/service/impl/ExpenseServiceImpl.java
+++ src/main/java/com/example/HouseholdAccountBook/service/impl/ExpenseServiceImpl.java
@@ -16,6 +16,27 @@ public class ExpenseServiceImpl implements ExpenseService {
     @Autowired
     ExpenseRepository expenseRepository;
 
+    public Response getExpenseByUserId(String userId) {
+        Response response = new Response();
+        try {
+            List<Expense> expenses = expenseRepository.findByUserId(userId);
+            // Dtoに詰め替え
+            MapperToDto mapperToDto = new MapperToDto();
+            List<ExpenseDto> expenseDtoList = new ArrayList<>();
+            expenses.forEach(expense ->
+                    expenseDtoList.add(mapperToDto.mapperToExpenseDto(expense))
+            );
+            response.setStatusCode(200);
+            response.setExpenseDtoList(expenseDtoList);
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("経費の取得に失敗しました。");
+            e.getStackTrace();
+        }
+
+        return response;
+    }
+
     public Response getExpenseByUserIdAndMonth(String userId, String month) {
         Response response = new Response();
         try {
